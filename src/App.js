e78ad31217167135375c0b583efe4c5d9898a468
saql829 <saqlain124@gmail.com>
2024-12-11 16:44:35 +0500

    simple add a buttons and design for a layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className='calculator'>
+        <form action="">
+          <div>
+             
+           <input type='text'></input>
+          </div>
+          <div>
+            <input type='button' value="Ac"/>
+            <input type='button' value="DE"/>
+            <input type='button' value="."/>
+            <input type='button' value="/"/>
+          </div>
+          <div>
+            <input type='button' value="7"/>
+            <input type='button' value="8"/>
+            <input type='button' value="9"/>
+            <input type='button' value="*"/>
+          </div>
+          <div>
+            <input type='button' value="6"/>
+            <input type='button' value="5"/>
+            <input type='button' value="4"/>
+            <input type='button' value="-"/>
+          </div>
+        </form>
+      </div>
     </div>
+    
   );
 }
 
